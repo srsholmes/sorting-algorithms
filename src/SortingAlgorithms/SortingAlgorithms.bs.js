a48fc3d0ff59ba33c63b9fba56ae7e5b5320a30d
srsholmes <srsholmes@gmail.com>
2019-12-14 08:02:03 +0000

    added in bs-css

--- src/SortingAlgorithms/SortingAlgorithms.bs.js
+++ src/SortingAlgorithms/SortingAlgorithms.bs.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var Css = require("bs-css/src/Css.js");
 var $$Array = require("bs-platform/lib/js/array.js");
 var Curry = require("bs-platform/lib/js/curry.js");
 var React = require("react");
@@ -15,6 +16,56 @@ function fill(element, length) {
   }
 }
 
+var card = Css.style(/* :: */[
+      Css.display(Css.flexBox),
+      /* :: */[
+        Css.flexDirection(Css.column),
+        /* :: */[
+          Css.alignItems(Css.stretch),
+          /* :: */[
+            Css.backgroundColor(Css.white),
+            /* :: */[
+              Css.boxShadow(Css.Shadow.box(undefined, Css.px(3), Css.px(5), undefined, undefined, Css.rgba(0, 0, 0, 0.3))),
+              /* :: */[
+                Css.unsafe("-webkit-overflow-scrolling", "touch"),
+                /* [] */0
+              ]
+            ]
+          ]
+        ]
+      ]
+    ]);
+
+var title = Css.style(/* :: */[
+      Css.fontSize(Css.rem(1.5)),
+      /* :: */[
+        Css.marginBottom(Css.px(10)),
+        /* [] */0
+      ]
+    ]);
+
+function actionButton(disabled) {
+  return Css.style(/* :: */[
+              Css.background(disabled ? Css.darkgray : Css.white),
+              /* :: */[
+                Css.color(Css.black),
+                /* :: */[
+                  Css.border(Css.px(1), Css.solid, Css.black),
+                  /* :: */[
+                    Css.borderRadius(Css.px(3)),
+                    /* [] */0
+                  ]
+                ]
+              ]
+            ]);
+}
+
+var Styles = {
+  card: card,
+  title: title,
+  actionButton: actionButton
+};
+
 function bob(param) {
   return React.createElement("div", {
               className: "value"
@@ -23,28 +74,35 @@ function bob(param) {
 
 function SortingAlgorithms(Props) {
   var match = React.useState((function () {
-          return 0;
+          return 15;
         }));
   var setListLength = match[1];
   var listLength = match[0];
   var myList = fill(bob(/* () */0), listLength);
-  return React.createElement("div", undefined, React.createElement("div", undefined, React.createElement("p", undefined, "Hello!"), React.createElement("p", undefined, "World!"), React.createElement("p", undefined, String(listLength)), React.createElement("input", {
-                      type: "range",
-                      value: String(listLength),
-                      onChange: (function ($$event) {
-                          var myVal = $$event.target.value;
-                          return Curry._1(setListLength, (function (listLength) {
-                                        return myVal;
-                                      }));
-                        })
-                    }), React.createElement("div", {
+  return React.createElement("div", undefined, React.createElement("p", undefined, "Hello!"), React.createElement("p", undefined, String(listLength)), React.createElement("h1", undefined, "Controls"), React.createElement("p", undefined, "Length of list to sort:"), React.createElement("input", {
+                  type: "range",
+                  value: String(listLength),
+                  onChange: (function ($$event) {
+                      var myVal = $$event.target.value;
+                      return Curry._1(setListLength, (function (listLength) {
+                                    return myVal;
+                                  }));
+                    })
+                }), React.createElement("div", undefined, React.createElement("h2", undefined, "Bubble Sort:"), React.createElement("div", {
                       className: "algoContainer"
-                    }, $$Array.of_list(myList))));
+                    }, $$Array.of_list(myList))), React.createElement("div", {
+                  className: card
+                }, React.createElement("h1", {
+                      className: title
+                    }, "Hello"), React.createElement("button", {
+                      className: actionButton(false)
+                    })));
 }
 
 var make = SortingAlgorithms;
 
 exports.fill = fill;
+exports.Styles = Styles;
 exports.bob = bob;
 exports.make = make;
-/* react Not a pure module */
+/* card Not a pure module */
--- src/SortingAlgorithms/SortingAlgorithms.re
+++ src/SortingAlgorithms/SortingAlgorithms.re
@@ -5,30 +5,62 @@ let rec fill = (~element: 'a, ~length: int) =>
     [element, ...fill(~element, ~length=length - 1)];
   };
 
+module Styles = {
+  /* Open the Css module, so we can access the style properties below without prefixing them with Css. */
+  open Css;
+
+  let card =
+    style([
+      display(flexBox),
+      flexDirection(column),
+      alignItems(stretch),
+      backgroundColor(white),
+      boxShadow(Shadow.box(~y=px(3), ~blur=px(5), rgba(0, 0, 0, 0.3))),
+      /* You can add non-standard and other unsafe style declarations using the `unsafe` function, with strings as the two arguments. */
+      unsafe("-webkit-overflow-scrolling", "touch"),
+      /* You can place all your theme styles in Theme.re and access as normal Reason module */
+    ]);
+
+  let title = style([fontSize(rem(1.5)), marginBottom(px(10))]);
+
+  let actionButton = disabled =>
+    style([
+      background(disabled ? darkgray : white),
+      color(black),
+      border(px(1), solid, black),
+      borderRadius(px(3)),
+    ]);
+};
+
 let bob = () => <div className="value"> {React.string("Hello")} </div>;
 
 [@react.component]
 let make = () => {
-  let (listLength, setListLength) = React.useState(() => 0);
+  let (listLength, setListLength) = React.useState(() => 15);
 
   let myList = fill(bob(), listLength);
-
   <div>
+    <p> {React.string("Hello!")} </p>
+    <p> {React.string(string_of_int(listLength))} </p>
+    <h1> {React.string("Controls")} </h1>
+    <p> {React.string("Length of list to sort:")} </p>
+    <input
+      value={string_of_int(listLength)}
+      onChange={event => {
+        let myVal = event->ReactEvent.Form.target##value;
+        setListLength(listLength => myVal);
+      }}
+      type_="range"
+    />
     <div>
-      <p> {React.string("Hello!")} </p>
-      <p> {React.string("World!")} </p>
-      <p> {React.string(string_of_int(listLength))} </p>
-      <input
-        value={string_of_int(listLength)}
-        onChange={event => {
-          let myVal = event->ReactEvent.Form.target##value;
-          setListLength(listLength => myVal);
-        }}
-        type_="range"
-      />
+      <h2> {React.string("Bubble Sort:")} </h2>
       <div className="algoContainer">
         {ReasonReact.array(Array.of_list(myList))}
       </div>
+    </div>
+    <div className=Styles.card>
+      <h1 className=Styles.title> {ReasonReact.string("Hello")} </h1>
+      <button className={Styles.actionButton(false)} />
     </div>
   </div>;
 };
